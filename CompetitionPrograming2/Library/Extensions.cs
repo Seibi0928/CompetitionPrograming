865c977f254e74a8370600a7dd9dcaa9c42a8285
haruanri <nanteneus@hotmail.co.jp>
2020-02-23 02:11:38 +0900

    新たにアルゴリズムとそのテストコードを追加

--- CompetitionPrograming2/Library/Extensions.cs
+++ CompetitionPrograming2/Library/Extensions.cs
@@ -1,110 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
-using static System.Math;
 
-namespace CompetitionPrograming2
+namespace CompetitionPrograming2.Library
 {
-    public sealed class Program : BaseProgram
-    {
-        public static void Main(string[] _) { using (new SetConsole()) { Solve(); } }
-        public static void Solve()
-        {
-            var nk = GetArray<long>();
-            var n = nk[0];
-            var k = nk[1];
-            if (n == 1 && k == 2)
-            {
-                Write(1); return;
-            }
-            Write(Math.Ceiling(Math.Log(n, k)));
-        }
-    }
-    public abstract class BaseProgram
-    {
-        protected readonly static long divisor = 1000000007;
-        protected static void Write(object obj) => Console.WriteLine(obj);
-        protected static string GetString()
-        {
-            var str = Console.ReadLine();
-            if (str == null) { throw new NullReferenceException("標準入力がnullです Solveメソッド内の解答コードが間違っています"); }
-            return str;
-        }
-        protected static T GetNumber<T>() where T : IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable
-        {
-            var str = GetString();
-            var t = typeof(T);
-            try
-            {
-                if ((t == typeof(byte)))
-                {
-                    return (T)(object)str.ToByte();
-                }
-                else if (t == typeof(int))
-                {
-                    return (T)(object)str.ToInt();
-                }
-                else if (t == typeof(long))
-                {
-                    return (T)(object)str.ToLong();
-                }
-                else if (t == typeof(double))
-                {
-                    return (T)(object)str.ToDouble();
-                }
-                else if (t == typeof(decimal))
-                {
-                    return (T)(object)str.ToDecimal();
-                }
-                else if (t == typeof(BigInteger))
-                {
-                    return (T)(object)str.ToBigInteger();
-                }
-            }
-            catch (OverflowException)
-            {
-                throw new OverflowException("より大きい数値型を指定してください");
-            }
-            throw new NotSupportedException();
-        }
-        protected static T[] GetArray<T>() where T : IComparable, IComparable<T>, IConvertible, IEquatable<T>
-        {
-            return GetString().ToArray<T>();
-        }
-        protected static List<T> GetList<T>() where T : IComparable, IComparable<T>, IConvertible, IEquatable<T>
-        {
-            return GetString().ToList<T>();
-        }
-        protected static TResult[] GetArray<TResult>(Func<string, TResult> selector)
-        {
-            return (TResult[])(object)GetString().Split().Select(selector).ToArray();
-        }
-        protected static void Swap<T>(ref T item1, ref T item2)
-        {
-            var tmp = item1;
-            item1 = item2;
-            item2 = tmp;
-        }
-        protected sealed class SetConsole : IDisposable
-        {
-            readonly StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-            public SetConsole()
-            {
-                sw.AutoFlush = false;
-                Console.SetOut(sw);
-            }
-            public void Dispose()
-            {
-                Console.Out.Flush();
-                sw.AutoFlush = true;
-                Console.SetOut(sw);
-            }
-        }
-    }
     public static class ExtentionsLibrary
     {
         public static T[] NextPermutation<T>(this IEnumerable<T> collection) where T : IComparable
@@ -115,7 +17,7 @@ namespace CompetitionPrograming2
             for (int i = 1; i < array.Length; i++)
             {
                 if (array[i - 1].CompareTo(array[i]) >= 0) { continue; }
-
+                
                 ReverseNum = array[i - 1];
                 ReverseNumIndex = i - 1;
             }
@@ -136,7 +38,7 @@ namespace CompetitionPrograming2
         public static IEnumerable<T[]> Permutations<T>(this IEnumerable<T> collection) where T : IComparable
         {
             var next = collection.NextPermutation();
-
+            
             while (!(next is null))
             {
                 yield return next;
@@ -268,14 +170,6 @@ namespace CompetitionPrograming2
                 else r = mid - 1;
             }
             return l;
-        }
-    }
-    public sealed class Settings
-    {
-        public enum Priority : byte
-        {
-            Smaller = 0,
-            Larger = 1
         }
     }
 }
---
+++ Test/LibraryTest/ExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using CompetitionPrograming2.Library;
+
+namespace Test.LibraryTest
+{
+    public class ExtensionsTest
+    {
+        public class NextPermutationTest
+        {
+            [Fact]
+            public void 次の辞書順の組み合わせを取得できること()
+            {
+                var tmp = new int[] { 1, 2, 3 };
+                Assert.Equal(new int[] { 1, 3, 2 }, tmp.NextPermutation());
+
+                tmp = new int[] { 1, 3, 2 };
+                Assert.Equal(new int[] { 2, 3, 1 }, tmp.NextPermutation());
+
+                tmp = new int[] { 2, 3, 1 };
+                Assert.Equal(new int[] { 3, 2, 1 }, tmp.NextPermutation());
+            }
+
+            [Fact]
+            public void 次の辞書順の組み合わせを取得できること2()
+            {
+                var tmp = new int[] { 2, 3, 1, 4 };
+                Assert.Equal(new int[] { 2, 3, 4, 1 }, tmp.NextPermutation());
+            }
+
+            [Fact]
+            public void 降順のとき次の辞書順の組み合わせは存在しないのでnullが返されること()
+            {
+                Assert.Null(new int[] { 3, 2, 1 }.NextPermutation());
+            }
+        }
+        
+
+        public class PermutationsTest
+        {
+            [Fact]
+            public void 順列を引数にした場合全ての組み合わせが辞書順で列挙されること()
+            {
+                var collections = new int[] { 1, 2, 3 }.Permutations();
+                Assert.Equal(new int[] { 1, 3, 2 }, collections.ElementAt(0));
+                Assert.Equal(new int[] { 2, 3, 1 }, collections.ElementAt(1));
+                Assert.Equal(new int[] { 3, 2, 1 }, collections.ElementAt(2));
+            }
+
+            [Fact]
+            public void 引数以降の組み合わせが辞書順で列挙されること()
+            {
+                var collections = new int[] { 1, 3, 2 }.Permutations();
+                Assert.Equal(new int[] { 2, 3, 1 }, collections.ElementAt(0));
+                Assert.Equal(new int[] { 3, 2, 1 }, collections.ElementAt(1));
+            }
+        }
+    }
+}
