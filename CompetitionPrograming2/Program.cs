7c78031997637817faebd173978ff5261ae4c8f0
e a <nanteneus@hotmail.co.jp>
2020-06-06 19:00:39 +0900

    IListを継承するオブジェクトからの分割変数代入に対応

--- CompetitionPrograming2/Program.cs
+++ CompetitionPrograming2/Program.cs
@@ -245,6 +245,61 @@ namespace CompetitionPrograming2
         {
             return source.OrderByDescending(keySelector, StringComparer.Ordinal);
         }
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            rest = self.Skip(1).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            rest = self.Skip(2).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out T third,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            third = self.Count > 2 ? self[2] : default;
+            rest = self.Skip(3).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out T third,
+            out T four,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            third = self.Count > 2 ? self[2] : default;
+            four = self.Count > 3 ? self[3] : default;
+            rest = self.Skip(4).ToArray();
+        }
     }
 
     public struct Mint
