a19ba850638bdc3a264eb8005a155428542e4e35
e a <nanteneus@hotmail.co.jp>
2020-06-06 18:54:25 +0900

    .Net Coreバージョン上げと配列からの分割代入対応

--- CompetitionPrograming2/Program.cs
+++ CompetitionPrograming2/Program.cs
@@ -10,7 +10,7 @@ namespace CompetitionPrograming2
     {
         public static void Main(string[] _) { using (new SetConsole()) { Solve(); } }
         public static void Solve()
-        {
+        {            
         }
     }
     public abstract class BaseProgram
@@ -286,6 +286,61 @@ namespace CompetitionPrograming2
         public static IOrderedEnumerable<TSource> OrderByDescending<TSource>(this IEnumerable<TSource> source, Func<TSource, string> keySelector)
         {
             return source.OrderByDescending(keySelector, StringComparer.Ordinal);
+        }
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            rest = self.Skip(1).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            rest = self.Skip(2).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out T third,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            third = self.Count > 2 ? self[2] : default;
+            rest = self.Skip(3).ToArray();
+        }
+
+        public static void Deconstruct<T>
+        (
+            this IList<T> self,
+            out T first,
+            out T second,
+            out T third,
+            out T four,
+            out IList<T> rest
+        )
+        {
+            first = self.Count > 0 ? self[0] : default;
+            second = self.Count > 1 ? self[1] : default;
+            third = self.Count > 2 ? self[2] : default;
+            four = self.Count > 3 ? self[3] : default;
+            rest = self.Skip(4).ToArray();
         }
     }
 
